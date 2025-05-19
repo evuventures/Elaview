4541963aa36876d1951fe5c39351ac53083de5f8
Michael-Anderson92 <Michael_Anderson92@hotmail.com>
2025-05-19 14:32:46 -0700

    configured front and back end config files. .env's need to be set up for each team member's folders Front end on localhost 3000, backend on localhost 3001. Can test backend at localhost:3000/api/hello for message

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   server: {
     port: 3000, // Ensures frontend runs on port 3000
     proxy: {
-      "/api": "http://localhost:3001", // Redirects API calls to the backend
+      "/api": "http://localhost:3000", // Redirects API calls to the backend
     }
   },
   build: {
