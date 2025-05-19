768b7c287596891a8350b7a5eda5c6499ff8824e
Harsh Gupta <harshgupta1064@gmail.com>
2025-05-19 22:04:49 +0530

    Initial Setup

---
+++ mobile_app/lib/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:mobile_app/navigation_menu.dart';
+import 'package:mobile_app/utils/constants/colors.dart';
+import 'package:mobile_app/utils/constants/text_strings.dart';
+import 'package:mobile_app/utils/theme/theme.dart';
+
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: TTexts.appName,
+      themeMode: ThemeMode.system,
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
+      debugShowCheckedModeBanner: false,
+      home: const Scaffold(bottomNavigationBar: NavigationMenu()),
+      );
+  }
+}
