1e3584127db4dbea49897d085b62335f69403ef0
Harsh Gupta <harshgupta1064@gmail.com>
2025-05-22 13:07:11 +0530

    SignIn

--- mobile_app/lib/utils/constants/colors.dart
+++ mobile_app/lib/utils/constants/colors.dart
@@ -43,4 +43,15 @@ class TColors {
   static const Color softGrey = Color(0xFFF4F4F4);
   static const Color lightGrey = Color(0xFFF9F9F9);
   static const Color white = Color(0xFFFFFFFF);
+
+  // Gradients
+  static const Gradient elaviewLogoGradient = LinearGradient(
+    colors: [
+      Color(0xFFBFA2F7), // pastel purple
+      Color(0xFFE6C6F7), // light purple
+      Color(0xFFF7C6E6), // pastel pink
+    ],
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+  );
 }
